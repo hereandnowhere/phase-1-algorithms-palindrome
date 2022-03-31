2883649cb6981655f53bfbc89683a5189a2f34a5
Jacob Vine <jvine1235@gmail.com>
2022-03-30 20:28:01 -0400

    Completed Palindrome problem

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(word){
+  const string = word;
+  const array = string.split('');
+  const reversed = [...array];
+  reversed.reverse();
+  const forward = array.join();
+  const backwards = reversed.join();
+  const palindrome = (forward === backwards);
+  return palindrome;
 }
 
+
+isPalindrome('abba');
 /* 
-  Add your pseudocode here
+  Assign word to variable 'string';
+  Assign string.split('') to variable 'array';
+  Use .reverse on a copy of array and save it in variable reversed
+  use .join on each variable to turn the array and reversed into a joined string
+  if forward === backwards is true than the it is a palindrome.
+
 */
 
 /*
-  Add written explanation of your solution here
+  To check if it is a palindrome, you want to compare the string characters in regular and reverse order.
+  So to keep it simple, just create two arrays out of the characters in the string.
+  One in the order entered, the other in reverse order.
+  We can use .reverse() after splitting the string into an array to flip the order of the word,
+  then use .join() to return the array to a word to check if array and reversed are the same.
+
 */
 
 // You can run `node index.js` to view these console logs
